f041ebc8b7daecb23e6c8eb21cb4acf3e612b4f4
Mash J Prime <mashjprime@gmail.com>
2019-11-07 20:47:43 +0800

    CS Files

---
+++ Roshambo/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+
+namespace Roshambo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Intro();
+            Menu();
+        }
+
+        public static void Menu()
+        {
+            //int selection;
+            string menuInput;            
+            String[] menuOptions;
+            menuOptions = new string[3];
+            menuOptions[0] = "1. Play Game";
+            menuOptions[1] = "2. High Score";
+            menuOptions[2] = "3. Exit";
+
+            Console.WriteLine("\n\nPlease select from the menu options below:");
+
+            for(int i = 0; i < menuOptions.Length; i++)
+            {
+                Console.WriteLine(menuOptions[i]);
+            }
+            Environment.Exit(0);
+            menuInput = Console.ReadLine();
+            while(menuInput != null)
+            {
+                menuInput = Console.ReadLine();
+            }
+
+
+        }
+
+        public static void Intro()
+        {
+            Console.WriteLine("Welcome to Roshambo");
+            Thread.Sleep(500);
+            Console.WriteLine("    The game of..");
+            Thread.Sleep(500);
+            Console.Write("Rock");
+            Thread.Sleep(500);
+            Console.Write("\tPaper");
+            Thread.Sleep(500);
+            Console.Write("\tScissors");
+        }
+
+
+    }
+}
