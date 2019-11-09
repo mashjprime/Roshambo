9df778bdedf8a31c23c1ca67b364ec4e03e77aa7
Mash J Prime <mashjprime@gmail.com>
2019-11-09 19:50:58 +0800

    Refactoring result system WIP

--- Roshambo/Program.cs
+++ Roshambo/Program.cs
@@ -6,10 +6,42 @@ namespace Roshambo
     class Program
     {
         public static int score = 0;
+        public static string[] weapons = {"Laser", "Rock", "Paper", "Scissors" };
         static void Main(string[] args)
         {
             Intro();
             Menu();
+        }
+
+
+        public static void rockArt()
+        {
+            Console.WriteLine("      _______");
+            Console.WriteLine("-- - '   ____)");
+            Console.WriteLine("      (_____)");
+            Console.WriteLine("      (_____)");
+            Console.WriteLine("      (____)");
+            Console.WriteLine("-- -.__(___)");
+        }
+
+        public static void paperArt()
+        {
+            Console.WriteLine("       _______");
+            Console.WriteLine("-- - '    ____)__");
+            Console.WriteLine("           ______)");
+            Console.WriteLine("          _______)");
+            Console.WriteLine("         _______)");
+            Console.WriteLine("---.__________)");
+        }
+
+        public static void scissorArt()
+        {
+            Console.WriteLine("    _______");
+            Console.WriteLine("---'   ____)____");
+            Console.WriteLine("          ______)");
+            Console.WriteLine("       __________)");
+            Console.WriteLine("      (____)");
+            Console.WriteLine("---.__(___)");
         }
 
 
@@ -20,55 +52,55 @@ namespace Roshambo
             return random.Next(1, 4);
         }
 
-        public static bool GameLogic(int input)
+        public static int GameLogic(int playerInput, int aiInput)
         {
-            bool win = false;
-            int aiNum = RandomNumber();
+            int result =0 ;
 
-            if(input == aiNum)
+            // Draw
+            if(playerInput == aiInput)
             {
-                Console.WriteLine("Draw");
-                win = false;
+                result = 0;
             }
             // Rock
-            else if(input == 1 && aiNum == 2) {
-                Console.WriteLine("Paper wraps rock");
-                win = false;
+            else if(playerInput == 1 && aiInput == 2) {
+                Console.WriteLine("Paper covers rock.. PC Wins");
+                result = 2;
             }
-            else if(input ==1 && aiNum == 3)
+            else if(playerInput ==1 && aiInput == 3)
             {
-                Console.WriteLine("Rock smashes scissors");
-                win = true;
+                Console.WriteLine("Rock smashes scissors..");
+                result = 1;
             }
             // Paper
-            else if(input == 2 && aiNum == 1)
+            else if(playerInput == 2 && aiInput == 1)
             {
-                Console.WriteLine("Paper wraps rock");
-                win = true;
+                Console.WriteLine("Paper covers rock");
+                result = 1;
             }
-            else if(input == 2 && aiNum == 3)
+            else if(playerInput == 2 && aiInput == 3)
             {
-                Console.WriteLine("Scissors cuts paper");
-                win = false;
+                Console.WriteLine("Scissors cuts paper.. PC Wins");
+                result = 2;
             }
             // Scissors
-            else if(input == 3 && aiNum == 1)
+            else if(playerInput == 3 && aiInput == 1)
             {
-                Console.WriteLine("Rock smashes scissors");
-                win = false;
+                Console.WriteLine("Rock smashes scissors.. PC Wins");
+                result = 2;
             }
-            else if(input == 3 && aiNum == 2)
+            else if(playerInput == 3 && aiInput == 2)
             {
                 Console.WriteLine("Scissors cuts paper");
-                win = true;
+                result = 1;
             }
-            return win;
+            return result;
         }
 
         public static void playGame()
         {
             string selection = null;
-            Console.WriteLine("\nTo play please enter a number corresponding to the options below:");
+            Console.Clear();
+            Console.WriteLine("To play please enter a number corresponding to the options below:");
             Console.WriteLine("1. Rock\n2. Paper\n3. Scissors");
 
             // Take input
@@ -92,12 +124,14 @@ namespace Roshambo
                         break;
                 }
 
-           // }
-            if (GameLogic(int.Parse(selection))) 
+            // }
+            /*if (GameLogic(int.Parse(selection)))
             {
                 //p1.SetHighScore(+1);
                 Console.WriteLine("You Win!!");
             }
+            pause(2);
+            Console.Clear();*/
 
         }
         public static void highScore()
@@ -115,15 +149,14 @@ namespace Roshambo
         public static void Menu()
         {
             string menuInput = null;            
-            String[] menuOptions;
-                    
+            String[] menuOptions;  
             menuOptions = new string[3];
             menuOptions[0] = "Play Game";
             menuOptions[1] = "High Score";
             menuOptions[2] = "Exit";
 
             // Display options
-            string msg = "\n\nPlease select from the menu options below:";
+            string msg = "Please select from the menu options below:";
 
             for (int i = 0; i < menuOptions.Length; i++)
             {
@@ -158,6 +191,18 @@ namespace Roshambo
 
         public static void Intro()
         {
+            // Intro animation
+            Console.Clear();
+            rockArt();
+            pause(1);
+            Console.Clear();
+            paperArt();
+            pause(1);
+            Console.Clear();
+            scissorArt();
+            pause(1);
+            Console.Clear();
+
             Console.WriteLine("Welcome to Roshambo");
             pause(1);
             Console.WriteLine("    The game of..");
@@ -171,9 +216,12 @@ namespace Roshambo
             player p1 = new player();
             Console.WriteLine("\nPlease enter your name:");
             string playerName = Console.ReadLine();
+            pause(1);
             p1.SetPlayerName(playerName);
             p1.SetHighScore(0);
             Console.WriteLine($"Welcome {p1.GetPlayerName()}");
+            pause(2);
+            Console.Clear();
         }
 
 
